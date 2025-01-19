ca55e4aa9129ad655c098df7698ab3f8db6288d8
devendersingh1999 <devendersingh29121999@gmail.com>
2025-01-19 20:32:53 +0530

    edit and add

--- left.js
+++ left.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // hi this is left file
 function add(){
   var a=2+ 5;
@@ -8,3 +9,26 @@ add();
 
 
   
+=======
+<<<<<<< HEAD
+// this is left file 
+=======
+// hi this is left file
+function add(){
+  var a=2+ 5;
+  console.log(a);  
+}
+add();
+
+
+
+  
+>>>>>>> newBranch
+
+const hey =()=>{
+
+    let reply= "hi";
+    console.log(reply);
+}
+hey();
+>>>>>>> Stashed changes
