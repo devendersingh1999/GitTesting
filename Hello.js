08ada17afa8922c29eb932cf5a143074f240f21e
devendersingh1999 <devendersingh29121999@gmail.com>
2025-01-19 13:42:15 +0530

    Hello file

---
+++ Hello.js
@@ -0,0 +1,31 @@
+console.log('hello world');
+<<<<<<< HEAD
+const hi =() =>{
+    console.log("this is hi funtion")
+  }
+  hi();
+=======
+
+const hi =() =>{
+  console.log("this is hi funtion")
+}
+hi();
+>>>>>>> 65174d245eaa0097954286cf3e3d3993efd9688c
+
+
+function tho() {
+  var sum = 1 + 1; // Corrected: 'var' is used to declare the variable
+  console.log(sum); // This will print the result
+}
+
+tho(); // Calling the function to see the result
+
+
+//im doing well learing github how to used git command, actually it is intersting and facinating
+
+function multi() {
+  var multiplication = 1* 3; 
+  console.log(multiplication);
+}
+multi();
+
